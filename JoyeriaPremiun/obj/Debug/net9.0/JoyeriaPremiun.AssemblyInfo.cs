20610b476439f32f1fb9bb27034ee1b77fe75d16
deijomes <deibermndoza@gmail.com>
2025-04-27 12:45:15 -0500

    ajuste

--- JoyeriaPremiun/obj/Debug/net9.0/JoyeriaPremiun.AssemblyInfo.cs
+++ JoyeriaPremiun/obj/Debug/net9.0/JoyeriaPremiun.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoyeriaPremiun")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77bc78a68d6af3fbededad12ae1fa84fd2d5d279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db07cafe2fec78b42fd8c102b3929609e374b8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoyeriaPremiun")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoyeriaPremiun")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
